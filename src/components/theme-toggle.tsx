b8ce64c7bcf848c9ffeea7f3c45deb25c485ad71
nafisaziziriza <nafisaziziriza@gmail.com>
2026-02-07 15:21:12 +1000

    refactor: mobile nav

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -23,7 +23,7 @@ export function ThemeToggle() {
       variant="ghost"
       size="icon"
       onClick={handleThemeToggle}
-      className="h-9 w-9 cursor-pointer"
+      className="cursor-pointer"
     >
       <Sun className="h-4 w-4 scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
       <Moon className="absolute h-4 w-4 scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
